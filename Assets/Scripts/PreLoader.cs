fdb68177acedb7f45fd0a96997ea5bac3221f86f
Salvador Galindo <sagalind@ucsc.edu>
2019-09-04 17:48:43 -0700

    added preload scene for loading persistant objects (like osund manager) and main menu scene with a single Play button

---
+++ Assets/Scripts/PreLoader.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PreLoader : MonoBehaviour
+{
+    private void Awake()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+    }
+}
